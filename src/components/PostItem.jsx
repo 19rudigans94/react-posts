9e54b1b63a0b07ff9361f499497bdf1f438aa0df
19rudigans94 <150382640+19rudigans94@users.noreply.github.com>
2024-08-26 20:37:59 +0500

    1

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -15,7 +15,7 @@ const PostItem = ({ post, number, remove }) => {
       </div>
       <div>
         <MyButton className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md "
-          onClick={() => remove(post)}>Удалить</MyButton>
+          onClick={() => remove(post.id)}>Удалить</MyButton>
       </div>
     </li>
   );
--- src/components/UI/button/MyButton.jsx
+++ src/components/UI/button/MyButton.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function MyButton({ children, onClick }) {
+export default function MyButton(props) {
   return (
-    <button className='my-button bg-red-500 hover:bg-red-600 rounded p-1' onClick={onClick}>{children}</button>
+    <button className='my-button bg-red-500 hover:bg-red-600 rounded p-1' onClick={props.onClick}>{props.children}</button>
   )
 }
